55c23ec179e0cd85be6e1daa532bc3a5a0cea87d
Angelos Kamariadis <150456060+akamariadis@users.noreply.github.com>
2025-10-31 14:12:25 +0200

    Update code2.s

--- 1η Σειρά Ασκήσεων/code2.s
+++ 1η Σειρά Ασκήσεων/code2.s
@@ -1,22 +1,34 @@
+.data
+x: .word 74 # el22674 -> last two digits = 74
+.text
+.globl main
+
+main:
+    lw  $a0, x        
+    jal BitCount        
+    move $a0, $v0
+    li   $v0, 1          
+    syscall
+    li   $v0, 10         
+    syscall
+    
 BitCount:
-    addi $sp, $sp, -8
-    sw   $ra, 4($sp)
-    sw   $t0, 0($sp)
-
-    beq  $a0, $0, base_case
-
-    andi $t0, $a0, 1
-    srl  $a0, $a0, 1
+    andi $s0, $a0, 1
+    addi $sp, $sp, -12
+    sw   $ra, 0($sp)
+    sw   $a0, 4($sp)
+    sw   $s0, 8($sp)
+    beq  $a0, $zero, return
+    srl  $a0, $a0, 1      
     jal  BitCount
-
-    add  $v0, $v0, $t0
-    j    BitCount_end
-
-base_case:
-    li   $v0, 0
-
-BitCount_end:
-    lw   $t0, 0($sp)
-    lw   $ra, 4($sp)
-    addi $sp, $sp, 8
+    lw   $s0, 8($sp)
+    lw   $a0, 4($sp)
+    lw   $ra, 0($sp)
+    addi $sp, $sp, 12
+    add  $v0, $v0, $s0     
+    jr   $ra
+    
+return:
+    add  $v0, $zero, $zero
+    addi $sp, $sp, 12
     jr   $ra
